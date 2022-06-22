8d00287ea0b85051672362a92af2e81d3a5eb674
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-22 20:17:46 +0530

    ADDED: use linkedlist to do the stack opertion

--- src/com/bridgelabs/StackAndQueue.java
+++ src/com/bridgelabs/StackAndQueue.java
@@ -1,7 +1,14 @@
 package com.bridgelabs;
 
-public class StackAndQueue {
+import java.util.LinkedList;
+
+public class StackAndQueue<E> {
     public static void main(String[] args) {
         System.out.println(" Welcome to Stack and Queue ");
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        stack.addFirst(70);
+        stack.addFirst(30);
+        stack.addFirst(56);
+        System.out.println(stack);
     }
 }
